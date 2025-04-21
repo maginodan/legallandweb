d47461d6e6fe166510c5dd673508ee401bcc305b
maginodan <137385172+maginodan@users.noreply.github.com>
2025-04-21 06:46:41 +0300

    Add files via upload

--- src/components/feature-section/index.tsx
+++ src/components/feature-section/index.tsx
@@ -1,4 +1,3 @@
-
 import { tw } from 'twind';
 import Check from '@/constants/svg/check.svg';
 
@@ -7,126 +6,64 @@ const FeatureSection = () => (
     <div className={tw(`max-w-7xl mx-auto p-4 sm:p-6 lg:p-8`)}>
       <div className={tw(`container mx-auto px-6 p-6 bg-white`)}>
         <div className={tw(`mb-16 text-center`)}>
-          <h4 className={tw(`text-base text-indigo-600 font-semibold tracking-wide uppercase`)}>
-            Services
-          </h4>
-          <p
-            className={tw(
-              `mt-2 text-5xl lg:text-7xl font-bold tracking-tight text-gray-900`
-            )}
-          >
+          <h4 className={tw(`text-base text-indigo-600 font-semibold tracking-wide uppercase`)}>Services</h4>
+          <p className={tw(`mt-2 text-5xl lg:text-7xl font-bold tracking-tight text-gray-900`)}>
             Empowering Secure Land Ownership
           </p>
         </div>
         <div className={tw(`flex flex-wrap my-12`)}>
-          <div
-            className={tw(
-              `w-full border-b md:w-1/2 md:border-r lg:w-1/3 p-8`
-            )}
-          >
+          <div className={tw(`w-full border-b md:w-1/2 md:border-r lg:w-1/3 p-8`)}>
             <div className={tw(`flex items-center mb-6`)}>
-              <Check
-                width={20}
-                height={20}
-                fill="currentColor"
-                className={tw(`h-6 w-6 text-indigo-500`)}
-              />
+              <Check width={20} height={20} fill="currentColor" className={tw(`h-6 w-6 text-indigo-500`)} />
               <div className={tw(`ml-4 text-xl`)}>Land Dispute Resolution</div>
             </div>
             <p className={tw(`leading-loose text-gray-500`)}>
-              Expert guidance resolves complex land disputes, ensuring fair outcomes
-              for all parties.
+              Expert guidance resolves complex land disputes, ensuring fair outcomes for all parties.
             </p>
           </div>
-          <div
-            className={tw(
-              `w-full border-b md:w-1/2 lg:w-1/3 lg:border-r p-8`
-            )}
-          >
+          <div className={tw(`w-full border-b md:w-1/2 lg:w-1/3 lg:border-r p-8`)}>
             <div className={tw(`flex items-center mb-6`)}>
-              <Check
-                width={20}
-                height={20}
-                fill="currentColor"
-                className={tw(`h-6 w-6 text-indigo-500`)}
-              />
+              <Check width={20} height={20} fill="currentColor" className={tw(`h-6 w-6 text-indigo-500`)} />
               <div className={tw(`ml-4 text-xl`)}>Title Registration Support</div>
             </div>
             <p className={tw(`leading-loose text-gray-500`)}>
-              Streamlined support secures land titles, protecting your ownership
-              rights in Uganda.
+              Streamlined support secures land titles, protecting your ownership rights in Uganda.
             </p>
           </div>
-          <div
-            className={tw(
-              `w-full border-b md:w-1/2 md:border-r lg:w-1/3 lg:border-r-0 p-8`
-            )}
-          >
+          <div className={tw(`w-full border-b md:w-1/2 md:border-r lg:w-1/3 lg:border-r-0 p-8`)}>
             <div className="flex items-center mb-6">
-              <Check
-                width={20}
-                height={20}
-                fill="currentColor"
-                className={tw(`h-6 w-6 text-indigo-500`)}
-              />
+              <Check width={20} height={20} fill="currentColor" className={tw(`h-6 w-6 text-indigo-500`)} />
               <div className={tw(`ml-4 text-xl`)}>Customary Tenure Expertise</div>
             </div>
             <p className={tw(`leading-loose text-gray-500`)}>
-              Specialized expertise navigates customary tenure, safeguarding
-              traditional land rights effectively.
+              Specialized expertise navigates customary tenure, safeguarding traditional land rights effectively.
             </p>
           </div>
-          <div
-            className={tw(
-              `w-full border-b md:w-1/2 lg:w-1/3 lg:border-r lg:border-b-0 p-8`
-            )}
-          >
+          <div className={tw(`w-full border-b md:w-1/2 lg:w-1/3 lg:border-r lg:border-b-0 p-8`)}>
             <div className={tw(`flex items-center mb-6`)}>
-              <Check
-                width={20}
-                height={20}
-                fill="currentColor"
-                className={tw(`h-6 w-6 text-indigo-500`)}
-              />
+              <Check width={20} height={20} fill="currentColor" className={tw(`h-6 w-6 text-indigo-500`)} />
               <div className={tw(`ml-4 text-xl`)}>Trusted Legal Advice</div>
             </div>
             <p className={tw(`leading-loose text-gray-500`)}>
-              Professional legal advice empowers confident decisions for land
-              ownership and security.
+              Professional legal advice empowers confident decisions for land ownership and security.
             </p>
           </div>
-          <div
-            className={tw(
-              `w-full border-b md:w-1/2 md:border-r md:border-b-0 lg:w-1/3 lg:border-b-0 p-8`
-            )}
-          >
+          <div className={tw(`w-full border-b md:w-1/2 md:border-r md:border-b-0 lg:w-1/3 lg:border-b-0 p-8`)}>
             <div className={tw(`flex items-center mb-6`)}>
-              <Check
-                width={20}
-                height={20}
-                fill="currentColor"
-                className={tw(`h-6 w-6 text-indigo-500`)}
-              />
+              <Check width={20} height={20} fill="currentColor" className={tw(`h-6 w-6 text-indigo-500`)} />
               <div className={tw(`ml-4 text-xl`)}>Leasehold Conflict Solutions</div>
             </div>
             <p className={tw(`leading-loose text-gray-500`)}>
-              Effective solutions address leasehold conflicts, ensuring clarity and
-              legal compliance in Uganda.
+              Effective solutions address leasehold conflicts, ensuring clarity and legal compliance in Uganda.
             </p>
           </div>
           <div className={tw(`w-full md:w-1/2 lg:w-1/3 p-8`)}>
             <div className={tw(`flex items-center mb-6`)}>
-              <Check
-                width={20}
-                height={20}
-                fill="currentColor"
-                className={tw(`h-6 w-6 text-indigo-500`)}
-              />
+              <Check width={20} height={20} fill="currentColor" className={tw(`h-6 w-6 text-indigo-500`)} />
               <div className={tw(`ml-4 text-xl`)}>Boundary Dispute Guidance</div>
             </div>
             <p className={tw(`leading-loose text-gray-500`)}>
-              Precise guidance resolves boundary disputes, protecting your land
-              rights with expert support.
+              Precise guidance resolves boundary disputes, protecting your land rights with expert support.
             </p>
           </div>
         </div>
